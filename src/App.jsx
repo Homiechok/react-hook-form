584907c167333daf672a9e9232418e2c73967767
Maksim Ushakov <jenadka@bk.ru>
2025-05-03 23:36:02 +0500

    feat: Add handle errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
 import "./App.css";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
-  }
+  };
 
   return (
     <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("email", {
-        required: true,
-        validate: value => value.includes("@"),
-      })} type="text" placeholder="Email" />
-      <input {...register("password", {
-        required: true,
-        minLength: 8,
-      })} type="password" placeholder="Password" />
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: (value) => {
+            if (!value.includes("@")) {
+              return "Email must include @";
+            }
+            return true;
+          },
+        })}
+        type="text"
+        placeholder="Email"
+      />
+      {errors.email && (
+        <div className="text-red-500">{errors.email.message}</div>
+      )}
+      <input
+        {...register("password", {
+          required: "Password is required",
+          minLength: {
+            value: 8,
+            message: "Password must be at least 8 characters",
+          },
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      {errors.password && (
+        <div className="text-red-500">{errors.password.message}</div>
+      )}
       <button type="submit">Submit</button>
     </form>
   );
