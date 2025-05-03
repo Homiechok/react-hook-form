f07d14e9692e13afe1155af4cc4be5db0849a4b6
Maksim Ushakov <jenadka@bk.ru>
2025-05-03 23:23:04 +0500

    feat: Add simple validate

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,14 @@ export default function App() {
 
   return (
     <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("email")} type="text" placeholder="Email" />
-      <input {...register("password")} type="password" placeholder="Password" />
+      <input {...register("email", {
+        required: true,
+        validate: value => value.includes("@"),
+      })} type="text" placeholder="Email" />
+      <input {...register("password", {
+        required: true,
+        minLength: 8,
+      })} type="password" placeholder="Password" />
       <button type="submit">Submit</button>
     </form>
   );
