45d4b983d65a0ed4c35dc6d4fa130e0d3f78360b
Maksim Ushakov <jenadka@bk.ru>
2025-05-03 23:15:52 +0500

    feat: Add react-hook-form base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import {useForm} from "react-hook-form";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState({
-    email: "",
-    password: "",
-  });
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setErrors({
-      email: "",
-      password: "",
-    });
-
-    // Manual validation
-    if (!email.includes("@")) {
-      setErrors({ ...errors, email: "Email must include @" });
-      return;
-    }
-
-    if (password.length < 8) {
-      setErrors({
-        ...errors,
-        password: "Password must be at least 8 characters",
-      });
-      return;
-    }
-
-    // Form submission
-    console.log("Form submitted!");
-  };
+  const onSubmit = (data) => {
+    console.log(data);
+  }
 
   return (
-    <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      {errors.email && <div className="text-pink-500">{errors.email}</div>}
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {errors.password && <div className="text-pink-500">{errors.password}</div>}
+    <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} type="text" placeholder="Email" />
+      <input {...register("password")} type="password" placeholder="Password" />
       <button type="submit">Submit</button>
     </form>
   );
