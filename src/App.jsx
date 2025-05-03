aec76a17b59fb494bd12f6540a7e80685c63f91d
Maksim Ushakov <jenadka@bk.ru>
2025-05-03 23:08:32 +0500

    feat: Add native form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,57 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="text-3xl font-bold underline">123</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors({
+      email: "",
+      password: "",
+    });
+
+    // Manual validation
+    if (!email.includes("@")) {
+      setErrors({ ...errors, email: "Email must include @" });
+      return;
+    }
+
+    if (password.length < 8) {
+      setErrors({
+        ...errors,
+        password: "Password must be at least 8 characters",
+      });
+      return;
+    }
+
+    // Form submission
+    console.log("Form submitted!");
+  };
+
+  return (
+    <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      {errors.email && <div className="text-pink-500">{errors.email}</div>}
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {errors.password && <div className="text-pink-500">{errors.password}</div>}
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
