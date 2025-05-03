58b96ed82b087a9ca575a1162033dff5f1c6c901
Maksim Ushakov <jenadka@bk.ru>
2025-05-04 00:21:16 +0500

    feat: Add default email and Schema validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,53 @@
 import "./App.css";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup/src/index.js";
 
 export default function App() {
+  const schema = yup
+    .object({
+      email: yup.string().email().required(),
+      password: yup.string().min(8).required(),
+    })
+    .required();
+
   const {
     register,
     handleSubmit,
     setError,
     formState: { errors, isSubmitting },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      email: "test@mail.ru",
+    },
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = async (data) => {
     try {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      throw new Error();
+      await new Promise((resolve) => setTimeout(resolve, 1000));
       console.log(data);
     } catch (error) {
       setError("root", {
         message: "This email is already taken",
-      })
+      });
     }
   };
 
   return (
     <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register("email", {
-          required: "Email is required",
-          validate: (value) => {
-            if (!value.includes("@")) {
-              return "Email must include @";
-            }
-            return true;
-          },
-        })}
-        type="text"
-        placeholder="Email"
-      />
+      <input {...register("email")} type="text" placeholder="Email" />
       {errors.email && (
         <div className="text-red-500">{errors.email.message}</div>
       )}
-      <input
-        {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 8,
-            message: "Password must be at least 8 characters",
-          },
-        })}
-        type="password"
-        placeholder="Password"
-      />
+      <input {...register("password")} type="password" placeholder="Password" />
       {errors.password && (
         <div className="text-red-500">{errors.password.message}</div>
       )}
       <button disabled={isSubmitting} type="submit">
         {isSubmitting ? "Loading..." : "Submit"}
       </button>
-      {errors.root && (
-        <div className="text-red-500">{errors.root.message}</div>
-      )}
+      {errors.root && <div className="text-red-500">{errors.root.message}</div>}
     </form>
   );
 }
